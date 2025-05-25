52e9fa1a2920495bbcc6cc1583b0d9ae8358e822
Grace Sewell <gsewell@bu.edu>
2025-05-24 21:27:28 -0400

    updated comments

--- quotes/views.py
+++ quotes/views.py
@@ -5,13 +5,13 @@ import time
 import random
 # Create your views here.
 
-quotes = [
+quotes = [ # list of quotes for app
         "I have an idea so smart that my head would explode if I even began to know what I was talking about.",
         "Shut up Meg.",
         "I may be an idiot, but there's one thing I'm not sir, and that is an idiot.",
     ]
 
-imgsrc = [
+imgsrc = [ # list of img url sources
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvOplh8cRMSKxCW4ViL_hJB6qhCTfA1siHn1T4gLfnL13Sr37Qczqvu0FDYreoxiBSS-0&usqp=CAU",
     "https://preview.redd.it/are-you-the-family-guy-because-youre-peter-griffin-or-are-v0-mxuao4bg49ec1.jpeg?auto=webp&s=40439ebd83c79abef4eeb1071c605c55ced68915",
     "https://static.wikia.nocookie.net/simpsons/images/4/4a/Profile_-_Peter_Griffin.png/revision/latest/thumbnail/width/360/height/450?cb=20250307004047",
--- restaurant/views.py
+++ restaurant/views.py
@@ -12,7 +12,7 @@ daily_special = [
 ]
 
 def main(request):
-
+    '''responds to url and delegates to main template'''
     template_name = 'restaurant/main.html'
 
     context = {
@@ -22,6 +22,7 @@ def main(request):
     return render(request, template_name, context)
 
 def restaurant_main(request):
+    '''responds to url and delegates to main template'''
 
     template_name = 'restaurant/main.html'
 
@@ -32,6 +33,9 @@ def restaurant_main(request):
     return render(request, template_name, context)
 
 def order(request):
+    '''responds to url and delegates to order template'''
+
+
     template_name = 'restaurant/order.html'
 
     context = {
@@ -42,45 +46,59 @@ def order(request):
     return render(request, template_name, context)
 
 def confirmation(request):
+    '''responds to url and delegates to confirmation template'''
+
+
     template_name = 'restaurant/confirmation.html'
     print(request.POST)
     currenttime = time.time()
     expectedtime = currenttime + (random.randint(30,60) * 60)
-
+    # calculates time by adding a random number of minutes from 30-60.
+    # multiply by 60 to get seconds in order to work with time func
     order = []
     total = 0
 
     if request.POST:
+        # function of this if loop is to see if each given element was selected 
+        # by the user or not to determine their order
 
         if 'hamburger' in request.POST:
             order += ['Hamburger']
             total += 4.99
+
         if 'cheeseburger' in request.POST:
             order += ['Cheeseburger']
             total += 5.99
+
         if 'fries' in request.POST:
             order += ['Fries']
             total += 3.99
+
         if 'cheese_fries' in request.POST:
             if 'Fries' in order:
                 order.remove('Fries')
                 total -= 3.99
+                # dont want duplicate fry orders
             order += ['Cheese Fries']
             total += 4.99
+
         if 'chili_cheese_fries' in request.POST:
             if 'Fries' in order:
                 order.remove('Fries')
                 total -= 3.99
+                # dont want duplicate fry orders
             order += ['Chili Cheese Fries']
             total += 5.99
+
         if 'drink' in request.POST:
             order += ['Soft Drink']
             total += 1.99
+
         if 'botd' in request.POST:
             order += ['Burger of the Day']
             total += 7.99
 
-        special = request.POST['special']
+        special = request.POST['special'] # did customer order a burger of the day?
         name = request.POST['name']
         phone = request.POST['phone']
         email = request.POST['email']
