0d72d8c7bd0a11dda1633151d54bae9661c5bd50
Grace Sewell <gsewell@bu.edu>
2025-05-27 10:12:06 -0400

    added show_all_profiles view and template

--- cs412/settings.py
+++ cs412/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'formdata',
     'restaurant',
     'blog',
-    'mini_fb'
+    'mini_fb',
 ]
 
 MIDDLEWARE = [
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Profile
+
 
 # Create your views here.
+
+class ShowAllProfilesView(ListView):
+    '''view class to show all profiles'''
+
+    model = Profile
+    template_name = 'mini_fb/show_all_profiles.html'
+    context_object_name = 'profiles'
